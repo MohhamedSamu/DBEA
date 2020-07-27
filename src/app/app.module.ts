30430981c3330674d254584ee3cf07df58ccdf62
Samuel Calderon <sfernandocalderon@gmail.com>
2020-07-27 16:46:08 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { DataService } from './data.service';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    AngularFireModule.initializeApp(environment.firebase, 'DBEA')
   ],
-  providers: [],
+  providers: [DataService], 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD6JQWhD5mK2LoxfR4rtBQsjtkMSYdbWeg",
+    authDomain: "dbea-362f3.firebaseapp.com",
+    databaseURL: "https://dbea-362f3.firebaseio.com",
+    projectId: "dbea-362f3",
+    storageBucket: "dbea-362f3.appspot.com",
+    messagingSenderId: "598316442773",
+    appId: "1:598316442773:web:22cacd803f3c9888a7279c",
+    measurementId: "G-WZ9BKXX411"
+  }
 };
 
 /*
