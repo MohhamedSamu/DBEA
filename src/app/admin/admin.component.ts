9c5ad67ced5f80862cd8bc8ad321eb01712191b2
Samuel Calderon <sfernandocalderon@gmail.com>
2020-10-01 09:26:12 -0600

    rol de admin

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppRoutingModule } from '../app-routing.module'
+import { DataService} from '../data.service'
+import {usuario} from '../models/models'
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
-
-  constructor() { }
+  user: firebase.User;
+  usuarios:usuario[];
+  constructor(public _data: DataService, private router : Router) { }
 
   ngOnInit(): void {
+    this._data.getUserState().subscribe(user => {
+      this.user = user;
+    })
+    this._data.getUsers().subscribe( usuarios => {
+      this.usuarios = usuarios;
+    })
+  }
+
+  esAdmin(){
+    for (let x = 0; x < this.usuarios.length; x++){
+      if (this.usuarios[x].id === this.user.uid){
+        if  (this.usuarios[x].role == 'admin'){
+          return true;
+        }
+      }
+    }
+    this.router.navigate(['']);
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-  
+// services
 import { DataService } from './data.service';
-import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+//components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule} from '@angular/fire/firestore'
-import { environment } from '../environments/environment';
 import { AdministracionDeGruposComponent } from './administracion-de-grupos/administracion-de-grupos.component';
 import { IngresonotasComponent } from './ingresonotas/ingresonotas.component';
 import { AsistenciaComponent } from './asistencia/asistencia.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AdminComponent } from './admin/admin.component';
+//modules
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule} from '@angular/fire/firestore'
+import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule, MatLabel} from '@angular/material/form-field';
+import {MatFormFieldModule } from '@angular/material/form-field';
 import {MatCardModule} from '@angular/material/card';
 import { ReactiveFormsModule } from '@angular/forms'
 import {MatButtonModule} from '@angular/material/button';
-import { RegisterComponent } from './register/register.component';
 import {MatSliderModule} from '@angular/material/slider';
-import { AdminComponent } from './admin/admin.component';
+import { MatExpansionModule} from '@angular/material/expansion';
 @NgModule({
   declarations: [
     AppComponent,
@@ -49,7 +52,8 @@ import { AdminComponent } from './admin/admin.component';
     MatFormFieldModule,
     MatCardModule,
     MatButtonModule,
-    MatSliderModule
+    MatSliderModule,
+    MatExpansionModule
   ],
   
   providers: [DataService],
